5baf8f3f474f275687b82190063a9d7e7c3237df
jeeva <jeevakumar4200@gmail.com>
2024-01-31 14:14:21 +0530

    this release is mergesf.c

--- mergesf.c
+++ mergesf.c
@@ -5,6 +5,10 @@
 #define MAX_FILES 3
 #define MAX_LINE_LENGTH 256
 
+int compareStrings(const void *a, const void *b) {
+    return strcmp(*(const char **)a, *(const char **)b);
+}
+
 void mergeAndSortFiles(const char *outputFileName, const char *inputFileNames[], int numFiles) {
     FILE *outputFile = fopen(outputFileName, "w");
     if (!outputFile) {
@@ -12,72 +16,32 @@ void mergeAndSortFiles(const char *outputFileName, const char *inputFileNames[],
         exit(EXIT_FAILURE);
     }
 
-    FILE *inputFiles[MAX_FILES];
+    char *lines[MAX_FILES * MAX_LINE_LENGTH];
+    size_t totalLines = 0;
+
     for (int i = 0; i < numFiles; i++) {
-        inputFiles[i] = fopen(inputFileNames[i], "r");
-        if (!inputFiles[i]) {
+        FILE *inputFile = fopen(inputFileNames[i], "r");
+        if (!inputFile) {
             perror("Error opening input file");
             exit(EXIT_FAILURE);
         }
+
+        char currentLine[MAX_LINE_LENGTH];
+
+        while (fgets(currentLine, sizeof(currentLine), inputFile)) {
+            lines[totalLines++] = strdup(currentLine);
+        }
+
+        fclose(inputFile);
     }
 
-    char currentLine[MAX_LINE_LENGTH];
-    char *lines[MAX_FILES] = {NULL};
+    qsort(lines, totalLines, sizeof(char *), compareStrings);
 
-    // Read the initial line from each file
-    for (int i = 0; i < numFiles; i++) {
-        if (fgets(currentLine, sizeof(currentLine), inputFiles[i])) {
-            lines[i] = strdup(currentLine);
-        } else if (feof(inputFiles[i])) {
-            // If a file is empty, set the line to NULL
-            lines[i] = NULL;
-        } else {
-            perror("Error reading from input file");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    while (1) {
-        int smallestIndex = -1;
-
-        // Find the smallest line
-        for (int i = 0; i < numFiles; i++) {
-            if (lines[i] && (smallestIndex == -1 || strcmp(lines[i], lines[smallestIndex]) < 0)) {
-                smallestIndex = i;
-            }
-        }
-
-        // If no lines are left, break the loop
-        if (smallestIndex == -1) {
-            break;
-        }
-
-        // Write the smallest line to the output file
-        fprintf(outputFile, "%s", lines[smallestIndex]);
-
-        // Read the next line from the file that had the smallest line
-        if (fgets(currentLine, sizeof(currentLine), inputFiles[smallestIndex])) {
-            free(lines[smallestIndex]);  // Free the memory allocated for the old line
-            lines[smallestIndex] = strdup(currentLine);
-        } else if (feof(inputFiles[smallestIndex])) {
-            // If the file is empty, set the line to NULL
-            free(lines[smallestIndex]);
-            lines[smallestIndex] = NULL;
-        } else {
-            perror("Error reading from input file");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    // Free memory for remaining lines
-    for (int i = 0; i < numFiles; i++) {
+    for (size_t i = 0; i < totalLines; i++) {
+        fprintf(outputFile, "%s", lines[i]);
         free(lines[i]);
     }
 
-    // Close files
-    for (int i = 0; i < numFiles; i++) {
-        fclose(inputFiles[i]);
-    }
     fclose(outputFile);
 }
 
